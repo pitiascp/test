3fa86273121200131efebd8433bd4c5f3d6c5633
Pitias <Pitias@Pitiass-Mac.local>
2016-09-25 10:47:24 +0700

    First commit

--- ClickClock/ViewController.swift
+++ ClickClock/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var lblTime: UILabel!
+    @IBOutlet weak var btnStop: UIButton!
+    var count: Int = 0
+    var gio: Int = 0
+    var phut: Int = 0
+    var timer : NSTimer!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +26,27 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    @IBAction func btnPlay(sender: AnyObject) {
+        timer = NSTimer.scheduledTimerWithTimeInterval(0.001, target: self, selector: Selector("text"), userInfo: nil, repeats: true)
+    }
+    
+    func text(){
+        count = count + 1
+        if (count == 100){
+            phut += 1
+            count = 0
+        }
+        if (phut == 60){
+            gio += 1
+            phut = 0
+        }
+        let a = String(gio) + ":" + String(phut) + ":" + String(count)
+        lblTime.text = a
+        
+    }
 
+    @IBAction func btnStop(sender: AnyObject) {
+    }
 
 }
 
